7f1250b68a044a2fff7c1a10817196a8962b380b
Ashraful Islam <theashrafislam@gmail.com>
2025-02-27 16:23:28 +0600

    navbar component desgin done

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex justify-between my-12'>
             {/* logo  */}
             <div>
-                <Image src={'/images/logo.png'} width={'100'} height={'100'} alt='hello'/>
+                <Image src="/images/logo.png" alt="hello" width={180} height={180}/>
             </div>
+
 
             {/* routes */}
             <div>
-                <ul>
-                    <li className='text-red-500'>Home</li>
-                    <li>Properties</li>
-                    <li>About us</li>
-                    <li>Blog</li>
-                    <button>Contact us</button>
+                <ul className='flex items-center gap-12'>
+                    <li className='text-[#030A1B] text-base font-medium  hover:font-semibold hover:text-[#FE753F] cursor-pointer'>Home</li>
+                    <li className='text-[#030A1B] text-base font-medium  hover:font-semibold hover:text-[#FE753F] cursor-pointer'>Properties</li>
+                    <li className='text-[#030A1B] text-base font-medium  hover:font-semibold hover:text-[#FE753F] cursor-pointer'>About us</li>
+                    <li className='text-[#030A1B] text-base font-medium  hover:font-semibold hover:text-[#FE753F] cursor-pointer'>Blog</li>
+                    <button className='px-[30px] py-[15px] font-medium text-base text-[#FFF] bg-[#2E6EFF] rounded'>Contact us</button>
                 </ul>
             </div>
         </div>
