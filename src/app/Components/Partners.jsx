451ece199c2ff2eb52ec4f6a358852a155a9b814
Ashraful Islam <theashrafislam@gmail.com>
2025-02-27 21:07:48 +0600

    partner section design is complate

--- src/app/Components/Partners.jsx
+++ src/app/Components/Partners.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 
 const Partners = () => {
     return (
-        <div>
-            <h1>Our Partners</h1>
-            <div>
-                <Image width={300} height={100} alt='hello' src={''}/>
-                <Image width={300} height={100} alt='hello' src={''}/>
-                <Image width={300} height={100} alt='hello' src={''}/>
+        <div className='mt-24 mb-36'>
+            <h1 className='text-[#030A1B] text-[44px] font-semibold text-center mb-16'>Our Partners</h1>
+            <div className='flex items-center justify-between'>
+                <Image width={120} height={50} alt='hello' src={'/images/1.png'}/>
+                <Image width={120} height={50} alt='hello' src={'/images/2.png'}/>
+                <Image width={120} height={50} alt='hello' src={'/images/3.png'}/>
+                <Image width={120} height={50} alt='hello' src={'/images/4.png'}/>
+                <Image width={120} height={50} alt='hello' src={'/images/5.png'}/>
             </div>
         </div>
     );
